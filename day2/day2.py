5d9d322655b048eb03d4934bdfe8268758079b47
Luuk Veenis <luukveenis@gmail.com>
2025-12-02 14:17:47 -0800

    Solve day 2 part 2
    Probably not the most efficient to regex it, but the answer is correct
    so *shrug*.

--- day2/day2.py
+++ day2/day2.py
@@ -1,23 +1,26 @@
 import sys
 import re
 
-def discover_invalid(id_range: str) -> list[int]:
+def discover_invalid(id_range: str, pattern: str) -> list[int]:
     start, end = id_range.split("-")
     invalid: list[int] = []
 
 
     for i in range(int(start), int(end) + 1):
-        match = re.search(r"^(\d+)\1$", str(i))
+        match = re.search(pattern, str(i))
         if match:
             invalid.append(i)
 
     return invalid
 
 with open(sys.argv[1], "r") as file:
-    result = 0
+    part1_result = 0
+    part2_result = 0
     input = file.read().strip()
 
     for id_range in input.split(","):
-        result += sum(discover_invalid(id_range))
+        part1_result += sum(discover_invalid(id_range, r"^(\d+)\1$"))
+        part2_result += sum(discover_invalid(id_range, r"^(\d+)\1+$"))
 
-    print(f"Part one: {result}")
+    print(f"Part one: {part1_result}")
+    print(f"Part two: {part2_result}")
