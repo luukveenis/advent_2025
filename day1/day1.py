4c719dd8b46c45a557ee54ae85f2b4c1bf4735fc
Luuk Veenis <luukveenis@gmail.com>
2025-12-02 13:41:24 -0800

    Update implementation for day 1 part 2

--- day1/day1.py
+++ day1/day1.py
@@ -2,20 +2,39 @@ class Lock:
     def __init__(self):
         self.position: int = 50
 
-    def rotate(self, op: str) -> int:
+    # Rotates the lock according to the given input
+    # Returns a tuple with the number of rotations past 0 and the final position of the lock
+    def rotate(self, op: str) -> tuple[int, int]:
         count = int(op[1:])
         adjustment = count if op[0] == "R" else -count
 
-        self.position = (self.position + adjustment) % 100
-        return self.position
+        rot, pos = divmod(self.position + adjustment, 100)
+        rotations = abs(rot)
+
+        # Edge cases:
+        # - when position + adjustment is exactly a multiple of 100 we double count the last move
+        # - when starting from 0 and turning left (negative), we need to ignore the first
+        #   rotation because we don't "pass" 0
+        if (adjustment > 0 and pos == 0) or (adjustment < 0 and self.position == 0):
+            rotations -= 1
+
+        self.position = pos
+
+        return (rotations, self.position)
 
 
 lock = Lock()
 
 with open("input.txt", "r") as file:
-    positions: list[int] = []
+    clicks = 0
+    points = 0
 
     for line in file:
-        positions.append((lock.rotate(line.strip())))
+        rotations, position = lock.rotate(line.strip())
 
-    print(positions.count(0))
+        clicks += rotations
+        if position == 0:
+            points += 1
+
+    print(f"Part one: {points}")
+    print(f"Part two: {points + clicks}")
