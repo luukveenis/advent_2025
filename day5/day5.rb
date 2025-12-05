66fc58371cda608ddc29fd624b12482c1a37cfc9
Luuk Veenis <luukveenis@gmail.com>
2025-12-05 14:36:51 -0800

    Solve day 5 part 2
    For this part the problem asks for the count of distinct fresh IDs. To
    solve this, we need to deduplicate (remove overlaps) from the input
    ranges.
    
    We do this by iterating over the ordered ranges provided and expanding
    the "current" range as long as there is overlap. When there is no longer
    overlap, we start a new range and continue expanding that one.

--- day5/day5.rb
+++ day5/day5.rb
@@ -1,6 +1,6 @@
 class InventoryManager
   def initialize(fresh, available)
-    @fresh = fresh
+    @fresh = deduplicate(fresh)
     @available = available
   end
 
@@ -8,8 +8,33 @@ class InventoryManager
     @available.select(&method(:is_fresh?))
   end
 
+  def fresh_count
+    @fresh.map(&:size).reduce(&:+)
+  end
+
   def is_fresh?(ingredient)
     @fresh.any? { |f| f.include?(ingredient) }
+  end
+
+  private
+
+  def deduplicate(fresh)
+    result = [fresh[0]]
+
+    fresh[1..].each do |range|
+      previous = result[-1]
+
+      if previous.cover?(range)
+        next
+      elsif previous.overlap?(range)
+        result.pop
+        result << (previous.first..range.last)
+      else
+        result << range
+      end
+    end
+
+    result
   end
 end
 
@@ -17,10 +42,10 @@ def parse_input(filename)
   content = File.read(filename)
   first_half, second_half = content.split("\n\n")
 
-  fresh = first_half.split("\n").map do |range|
+  fresh = first_half.split("\n").map { |range|
     from, to = range.split("-")
     (from.to_i..to.to_i)
-  end
+  }.sort_by(&:first)
   available = second_half.split("\n").map(&:to_i)
 
   [fresh, available]
@@ -30,3 +55,4 @@ fresh, available = parse_input(ARGV[0])
 manager = InventoryManager.new(fresh, available)
 
 puts "Part 1: #{manager.available_fresh.size }"
+puts "Part 2: #{manager.fresh_count}"
