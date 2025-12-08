eaaf069e4e4af3220db53e8de8c5cdc460df5af7
Luuk Veenis <luukveenis@gmail.com>
2025-12-08 14:21:01 -0800

    Solve day 6 part 2
    This is pretty gross, but I'm not going to optimize it any more.
    
    For part 1 we could just split the lines to get the values and transpose
    those numbers to get the expressions to evaluate. For part 2, we need to
    do something similar except effectively "rotate" the entire input 90
    degrees counter clockwise retaining column positions of characters and
    then read line by line. This way we know blank lines separate each set
    of operations.

--- day6/day6.py
+++ day6/day6.py
@@ -1,21 +1,49 @@
 import sys
 from functools import reduce
+from itertools import groupby
 
 # Turns the input matrix into a list of standard mathematical operations
 # ie: [[1, 2], [3, 4], [+, *]] -> ["1 + 2", "3 * 4"]
-def build_expressions(input: list[list[str]]) -> list[str]:
-    transposed = list(zip(*input))
+def build_expressions(input: list[str]) -> list[str]:
+    words = [line.split() for line in input]
+    transposed = list(zip(*words))
     return [f" {t[-1]} ".join(t[:-1]) for t in transposed]
+
+# Similar to part 1 above, except we need to transpose the characters to
+# effectively "rotate" the input 90 degrees counter clockwise
+def rtl_expressions(input: list[str]) -> list[str]:
+    chars = [list(l) for l in input]
+    transposed = list(zip(*chars))[::-1]
+    lines = ["".join(l).strip() for l in transposed]
+
+    grouped = groupby(lines, key=lambda x: x == "")
+    groups = [list(group) for is_empty, group in grouped if not is_empty]
+
+    expressions = []
+
+    for group in groups:
+        operator = group[-1][-1]
+        group[-1] = group[-1][:-1]
+
+        expressions.append(f" {operator} ".join(group))
+
+    return expressions
+
+# Take a list of math expressions, evaluate them, and sum the results
+def evaluate(expressions: list[str]) -> int:
+    return reduce(lambda acc, t: acc + eval(t), expressions, 0)
+
 
 
 lines = []
 
 with open(sys.argv[1], "r") as file:
     for line in file:
-        lines.append(line.strip().split())
+        lines.append(line.rstrip("\n"))
 
-expressions = build_expressions(lines)
-part1_result = reduce(lambda acc, t: acc + eval(t), expressions, 0)
+part1_expressions = build_expressions(lines)
+part2_expressions = rtl_expressions(lines)
 
-print(f"Part 1: {part1_result}")
+print(f"Part 1: {evaluate(part1_expressions)}")
+print(f"Part 2: {evaluate(part2_expressions)}")
 
