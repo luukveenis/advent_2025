b6424f17364db066cfcef3b0e749227aff2fde37
Luuk Veenis <luukveenis@gmail.com>
2025-12-09 13:29:01 -0800

    Add naive DFS algorithm for day 7 part 2
    This works for the example, but the recurse depth is too much for the
    input and won't complete. Going to redo this with an adjacency matrix.

--- day7/day7.py
+++ day7/day7.py
@@ -20,17 +20,31 @@ def count_splits(input: list[list[str]]) -> int:
 
     return splits
 
+def follow_timeline(input: list[list[str]], i: int, j: int) -> int:
+    if (i == len(input) - 1):
+        return 1
+    elif (input[i][j] == "."):
+        return follow_timeline(input, i + 1, j)
+    elif (input[i][j] == "^"):
+        return follow_timeline(input, i, j - 1) + follow_timeline(input, i, j + 1)
+    else:
+        return 0
 
-
-
+# Part 2 is just path counting, we do a DFS using the #follow_timeline method
+# to count the total number of paths a tachyon can follow to the bottom
+def quantum(input: list[list[str]]) -> int:
+    start = input[0].index("S")
+    return follow_timeline(input, 1, start)
 
 
 with open(sys.argv[1], "r") as file:
     input = [list(line.strip()) for line in file]
 
     splits = count_splits(input)
+    timelines = quantum(input)
 
 
     print()
     print(f"Part 1: {splits}")
+    print(f"Part 2: {timelines}")
 
